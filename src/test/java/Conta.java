b8570e4b9b1abcf9668e7493fbd0c82f20f6d480
Mateus de Queiroz Davidson <84162653+MQueirozD@users.noreply.github.com>
2022-11-11 21:19:16 -0300

    Atualizado

--- src/test/java/Conta.java
+++ src/test/java/Conta.java
@@ -4,29 +4,12 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Conta {
-	
-	public boolean clienteEspecial = false;
+
+	public boolean clienteEspecial = true;
 	public int conta;
 	public int saque;
 	public int saldo;
 
-	public void cliente() {
-		/**
-		 *  aqui vemos se o cliente é especial ou não
-		 */
-		
-
-		if (this.clienteEspecial == true) {
-			um_cliente_especial_com_saldo_atual_de_reais(conta);
-			for_solicitado_um_saque_no_valor_de_reais(saque);
-			deve_efetuar_o_sque_e_atualizar_o_saldo_da_conta_para_reais(saldo);
-		} else {
-			um_cliente_commum_com_saldo_atual_de_reais(conta);
-			solicitar_um_saque_de_reais(saque);
-			nao_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente();
-			System.out.print("entrou");
-		}
-	}
 
 	/**
 	 * 
@@ -35,40 +18,42 @@ public class Conta {
 	@Given("Um cliente especial com saldo atual de {int} reais")
 	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
 		// Given Um cliente especial com saldo atual de -200 reais
-		this.conta = conta;
+		this.conta = int1;
 
-		if (conta instanceof Integer && this.clienteEspecial == true) {
-		} else
-		throw new io.cucumber.java.PendingException();
+		if (int1 instanceof Integer && this.clienteEspecial == true) {
+		} else {
+			throw new io.cucumber.java.PendingException();			
+		}
 	}
 
 	/**
 	 * 
-	 * @param int1 Verifica se e um cliente especial, para autorizar ou nao o valor   do saque
+	 * @param int1 Verifica se e um cliente especial, para autorizar ou nao o valor
+	 *             do saque
 	 */
 	@When("For solicitado um saque no valor de {int} reais")
-	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {		
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
 		// When for solicitado um saque no valor de 100 reais
-		this.saque = saque;
+		this.saque = int1;
 
-		if (saque instanceof Integer && this.clienteEspecial) {
+		if (int1 instanceof Integer && this.clienteEspecial) {
 		} else
 
-		throw new io.cucumber.java.PendingException();
+			throw new io.cucumber.java.PendingException();
 	}
 
 	/**
 	 * 
-	 * @param int1 Se o conta for de um cliente especial, autoriza a saque
+	 * @param int1 Se o conta for de um cliente especial, autoriza o saque
 	 */
 
 	@Then("Deve efetuar o sque e atualizar o saldo da conta para {int} reais.")
 	public void deve_efetuar_o_sque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
 		// Then deve efetuar o saque e atualizar o saldo da conta para -300 reais
-		if (saldo instanceof Integer && this.clienteEspecial == true) {
-			saldo = this.conta - (this.saque);
+		if (int1 instanceof Integer && this.clienteEspecial == true) {
+			int1 = this.conta - (this.saque);
 		} else
-		throw new io.cucumber.java.PendingException();
+			throw new io.cucumber.java.PendingException();
 	}
 
 	/**
@@ -79,12 +64,12 @@ public class Conta {
 	@Given("Um cliente commum com saldo atual de {int} reais")
 	public void um_cliente_commum_com_saldo_atual_de_reais(Integer int1) {
 		// Given um cliente comum com saldo atual de -300 reais
-		this.conta = conta;
+		this.conta = int1;
 
-		if (conta instanceof Integer && this.clienteEspecial == false) {
+		if (int1 instanceof Integer && this.clienteEspecial == false) {
 		} else
-			
-		throw new io.cucumber.java.PendingException();
+
+			throw new io.cucumber.java.PendingException();
 	}
 
 	/**
@@ -95,20 +80,19 @@ public class Conta {
 	@When("solicitar um saque de {int} reais")
 	public void solicitar_um_saque_de_reais(Integer int1) {
 		// When solicitar um saque de 200 reais
-		this.saque = saque;
+		this.saque = int1;
 
-		if (saque instanceof Integer) {
+		if (int1 instanceof Integer) {
 		} else
 
-		throw new io.cucumber.java.PendingException();
+			throw new io.cucumber.java.PendingException();
 	}
 
 	/**
 	 * 
-	 * @param int1 Se a conta estiver com valor mairo que zero
-	 * com o saque menor que a conta 
-	 * e for um cliente comum pode sacar.
-	 * Caso não entre em nunhuma dessas opções, não autorizar o saque
+	 * @param int1 Se a conta estiver com valor mairo que zero com o saque menor que
+	 *             a conta e for um cliente comum pode sacar. Caso não entre em
+	 *             nunhuma dessas opções, não autorizar o saque
 	 */
 	@Then("Nao deve efetuar o saque e deve retornar a mensagem saldo insuficiente")
 	public void nao_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente(Integer int1) {
@@ -117,7 +101,7 @@ public class Conta {
 
 		} else
 
-		throw new io.cucumber.java.PendingException();
+			throw new io.cucumber.java.PendingException();
 	}
 
 }
